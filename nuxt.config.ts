d663984ffa76223da5e23aa9a01306d82b2ede58
EDM115 <dev@edm115.eu.org>
2025-04-30 01:23:49 +0200

    feat(0.1.0): all basic configs done - ignore lockfile - added OxLint - base files so it builds - TS ESLint with more rules - highly customized Nuxt config - added necessary deps - added more scripts and use nuxi - added type checking - did some PNPM trickery to make sure everything works (namely hoist node linker and shamefully hoist) - better TS config - added VS Code recommended extensions and settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,81 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/ui'
-  ]
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxt/ui"
+  ],
+  devtools: {
+    enabled: true,
+    timeline: { enabled: true },
+    vueDevTools: true
+  },
+  app: {
+    pageTransition: { name: "page", mode: "out-in" }
+  },
+  css: [ "~/assets/styles/main.scss" ],
+  devServer: {
+    port: 8888
+  },
+  future: {
+    typescriptBundlerResolution: true
+  },
+  experimental: {
+    asyncContext: true,
+    buildCache: true,
+    clientFallback: true,
+    clientNodeCompat: true,
+    crossOriginPrefetch: true,
+    defaults: {
+      nuxtLink: {
+        prefetch: true,
+        prefetchOn: { interaction: true, visibility: false }
+      }
+    },
+    normalizeComponentNames: true,
+    sharedPrerenderData: true,
+    typedPages: true,
+    viewTransition: true,
+    writeEarlyHints: true
+  },
+  compatibilityDate: "2024-11-01",
+  nitro: {
+    compressPublicAssets: {
+      brotli: true,
+      gzip: true
+    },
+    esbuild: {
+      options: {
+        target: "esnext"
+      }
+    },
+    minify: true,
+    rollupConfig: {
+      output: {
+        compact: true,
+        /* manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id.split("node_modules/").pop()?.split("/")[0]
+          }
+
+          return id.toString().split("/")[0]
+        }, */
+        validate: true
+      }
+    }
+  },
+  vite: {
+    clearScreen: false
+  },
+  typescript: {
+    typeCheck: true
+  },
+  eslint: {
+    config: {
+      autoInit: false,
+      standalone: false
+    }
+  }
 })
